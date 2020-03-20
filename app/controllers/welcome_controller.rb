403823dcadccc5080e87a95242bf44d2912a03df
Dan Evers <dle1967@gmail.com>
2020-03-20 17:33:11 -0400

    Group by countries with/without states

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,12 @@ class WelcomeController < ApplicationController
 
     puts "apiSourceCode: " + ojs[ "apiSourceCode" ]
 
+    puts "---"
     puts "entry count=" + ojs[ "rawData" ].length.to_s
     countrySummaries  = {}
+
+    puts "---"
+    puts "standalone countries"
     ojs[ "rawData" ].each do |oneRawValue|
       if oneRawValue[ "Province/State" ].length > 0
         if countrySummaries.key?( oneRawValue[ "Country/Region" ])
@@ -17,10 +21,17 @@ class WelcomeController < ApplicationController
           countrySummaries[ oneRawValue[ "Country/Region" ]] = { "Confirmed" => oneRawValue[ "Confirmed" ].to_i }
         end
 
-        puts "running one.Country/Region=" + oneRawValue[ "Country/Region" ] + " add=" + oneRawValue[ "Confirmed" ].to_s + "  confirmed=" + countrySummaries[ oneRawValue[ "Country/Region" ]][ "Confirmed" ].to_s
+        # puts "running one.Country/Region=" + oneRawValue[ "Country/Region" ] + " add=" + oneRawValue[ "Confirmed" ].to_s + "  confirmed=" + countrySummaries[ oneRawValue[ "Country/Region" ]][ "Confirmed" ].to_s
       else
         puts "one.Country/Region=" + oneRawValue[ "Country/Region" ] + "  confirmed=" + oneRawValue[ "Confirmed" ]
       end
+    end
+
+    # Now for all the once that saw accumulated province/state(s)
+    puts "---"
+    puts "countries with provinces/states"
+    countrySummaries.each do |oneKey, oneValue|
+      puts "one.accumulated.Country/Region=" + oneKey + " total confirmed=" + countrySummaries[ oneKey ][ "Confirmed" ].to_s
     end
   end
 end
