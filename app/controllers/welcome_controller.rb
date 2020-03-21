8b8c84857964f636f6e319643369baddf9341b72
Dan Evers <dle1967@gmail.com>
2020-03-21 11:53:07 -0400

    Parse an input file; sample google chart

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,27 @@
 class WelcomeController < ApplicationController
   def index
     dataPathBase  = "/Users/dlevers/Src/Sandbox/Coronavirus19/data/JohnsHopkinsPipedream/"
-    fileName      = "2020-03-20-13.22.json"
-    fjs = File.read( dataPathBase + fileName )
+    # fileName      = "2020-03-20-13.22.json"
+    allSummary  = { "Confirmed" => 0 }
+
+    Dir.foreach( dataPathBase ) do |filename|
+      next if filename == '.' or filename == '..'
+      # Do work on the remaining files & directories
+      allSummary[ "Confirmed" ] += indexOneFile( dataPathBase, filename )
+    end
+    puts "---"
+    puts "allSummary.Confirmed=" + allSummary[ "Confirmed" ].to_s
+  end
+
+  private
+  def indexOneFile( pathIn, filenameIn )
+    fjs = File.read( pathIn + filenameIn )
     ojs = JSON.parse( fjs )
 
+    totalConfirmed  = 0
+
+    puts "pathIn:        " + pathIn
+    puts "filenameIn:    " + filenameIn
     puts "apiSourceCode: " + ojs[ "apiSourceCode" ]
 
     puts "---"
@@ -24,14 +41,18 @@ class WelcomeController < ApplicationController
         # puts "running one.Country/Region=" + oneRawValue[ "Country/Region" ] + " add=" + oneRawValue[ "Confirmed" ].to_s + "  confirmed=" + countrySummaries[ oneRawValue[ "Country/Region" ]][ "Confirmed" ].to_s
       else
         puts "one.Country/Region=" + oneRawValue[ "Country/Region" ] + "  confirmed=" + oneRawValue[ "Confirmed" ]
+        totalConfirmed  += oneRawValue[ "Confirmed" ].to_i
       end
     end
 
-    # Now for all the once that saw accumulated province/state(s)
+    # Now for all those that saw accumulated province/state(s)
     puts "---"
     puts "countries with provinces/states"
     countrySummaries.each do |oneKey, oneValue|
       puts "one.accumulated.Country/Region=" + oneKey + " total confirmed=" + countrySummaries[ oneKey ][ "Confirmed" ].to_s
+      totalConfirmed += countrySummaries[ oneKey ][ "Confirmed" ]
     end
+
+    return totalConfirmed
   end
 end
