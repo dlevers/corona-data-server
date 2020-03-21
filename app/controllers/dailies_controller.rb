aa7ac16c03aca22f663549af187f7cb1528896eb
Dan Evers <dle1967@gmail.com>
2020-03-21 17:22:16 -0400

    One level of drilldown

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -1,8 +1,15 @@
 class DailiesController < ApplicationController
   def show
-    @daily = Daily.find( params[ :id ])
-    puts "show: daily.territory=" + @daily.territory
+    daily = Daily.find( params[ :id ])
+    puts "show: daily.territory=" + daily.territory
+    @parentTerritory = daily.territory
 
-    @dailies = Daily.where( :territoryparent => @daily.territory )
+    @dailies = Daily.where( :territoryparent => daily.territory )
+
+    allTerritories  = []
+    @dailies.each do |oneDaily|
+        allTerritories.append( oneDaily.territory )
+    end
+    @uTerritories = allTerritories.uniq()
   end
 end
