f29805d86aa0d610afddf9c86d3853bf684553c0
Dan Evers <dle1967@gmail.com>
2020-03-21 16:57:36 -0400

    Add Show function

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -1,2 +1,8 @@
 class DailiesController < ApplicationController
+  def show
+    @daily = Daily.find( params[ :id ])
+    puts "show: daily.territory=" + @daily.territory
+
+    @dailies = Daily.where( :territoryparent => @daily.territory )
+  end
 end
