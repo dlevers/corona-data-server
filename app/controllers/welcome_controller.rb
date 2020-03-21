e7c6fef90c946b4c09c9391a914bd471ae5a2a27
Dan Evers <dle1967@gmail.com>
2020-03-21 14:25:22 -0400

    Controller Index method loads new json files to db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,34 +1,39 @@
 class WelcomeController < ApplicationController
   KExpectedDatestringLength = 10
+  KTerritoryWorld           = "world"
 
   def index
     dataPathBase  = "/Users/dlevers/Src/Sandbox/Coronavirus19/data/JohnsHopkinsPipedream/"
     # fileName      = "2020-03-20-13.22.json"
-    allSummary  = { "Confirmed" => 0 }
+    #allSummary  = { "Confirmed" => 0 }
+    allSummaryConfirmed = 0
 
     Dir.foreach( dataPathBase ) do |filename|
       next if filename == '.' or filename == '..'
+
+      # Do work on the remaining files & directories
       dateString    = dateFromFilename( filename )
       puts "index: dateString=" + dateString
       if dateString.length == KExpectedDatestringLength
-        #dailies = Daily.find( @date ).where( :date => dateString )
+        # Valid, keep going
         dailies = Daily.find_by( :date => dateString )
         if !dailies
-          puts "index: ZERO dailies.length"
+          puts "index: keep for ZERO dailies.length"
+          #allSummary[ "Confirmed" ] += indexOneFile( dataPathBase, filename )
+          allSummaryConfirmed = indexOneFile( dateString, dataPathBase, filename )
+          puts "index: allSummaryConfirmed=" + allSummaryConfirmed.to_s
         else
-          puts "index: dailies.length=" + dailies.length
+          puts "index: SKIP for existing dailies"
         end
-
-        # # Do work on the remaining files & directories
-        # allSummary[ "Confirmed" ] += indexOneFile( dataPathBase, filename )
       else
         puts "index: ERROR dateString=" + dateString
       end
     end
 
     puts "---"
-    puts "allSummary.Confirmed=" + allSummary[ "Confirmed" ].to_s
+    puts "allSummary.Confirmed=" + allSummaryConfirmed.to_s
   end
+
 
   private
 
@@ -45,15 +50,17 @@ class WelcomeController < ApplicationController
   end
 
 
-  def indexOneFile( pathIn, filenameIn )
+  def indexOneFile( datestringIn, pathIn, filenameIn )
     fjs = File.read( pathIn + filenameIn )
     ojs = JSON.parse( fjs )
 
-    totalConfirmed  = 0
+    totals  = { "Confirmed" => 0,
+                "Recovered" => 0,
+                "Deaths" => 0 }
 
     puts "pathIn:        " + pathIn
     puts "filenameIn:    " + filenameIn
-    puts "apiSourceCode: " + ojs[ "apiSourceCode" ]
+    # puts "apiSourceCode: " + ojs[ "apiSourceCode" ]
     puts "entry count:   " + ojs[ "rawData" ].length.to_s
     countrySummaries  = {}
 
@@ -63,15 +70,43 @@ class WelcomeController < ApplicationController
       if oneRawValue[ "Province/State" ].length > 0
         if countrySummaries.key?( oneRawValue[ "Country/Region" ])
           countrySummaries[ oneRawValue[ "Country/Region" ]][ "Confirmed" ] += oneRawValue[ "Confirmed" ].to_i
-        else
-          countrySummaries[ oneRawValue[ "Country/Region" ]] = { "Confirmed" => oneRawValue[ "Confirmed" ].to_i }
-        end
+          countrySummaries[ oneRawValue[ "Country/Region" ]][ "Recovered" ] += oneRawValue[ "Recovered" ].to_i
+          countrySummaries[ oneRawValue[ "Country/Region" ]][ "Deaths" ]    += oneRawValue[ "Deaths" ].to_i
 
-        # puts "running one.Country/Region=" + oneRawValue[ "Country/Region" ] + " add=" + oneRawValue[ "Confirmed" ].to_s + "  confirmed=" + countrySummaries[ oneRawValue[ "Country/Region" ]][ "Confirmed" ].to_s
+          latitude  = countrySummaries[ oneRawValue[ "Country/Region" ]][ "Latitude" ] * countrySummaries[ oneRawValue[ "Country/Region" ]][ "count" ]
+          latitude  += oneRawValue[ "Latitude" ].to_f
+          latitude  /= countrySummaries[ oneRawValue[ "Country/Region" ]][ "count" ] + 1
+          countrySummaries[ oneRawValue[ "Country/Region" ]][ "Latitude" ]   = latitude
+          longitude = countrySummaries[ oneRawValue[ "Country/Region" ]][ "Longitude" ] * countrySummaries[ oneRawValue[ "Country/Region" ]][ "count" ]
+          longitude += oneRawValue[ "Longitude" ].to_f
+          longitude /= countrySummaries[ oneRawValue[ "Country/Region" ]][ "count" ] + 1
+          countrySummaries[ oneRawValue[ "Country/Region" ]][ "Longitude" ]  = longitude
+
+          countrySummaries[ oneRawValue[ "Country/Region" ]][ "count" ]     += 1
+        else
+          countrySummaries[ oneRawValue[ "Country/Region" ]] = { "count" => 1,
+                                                                "Confirmed" => oneRawValue[ "Confirmed" ].to_i,
+                                                                "Recovered" => oneRawValue[ "Recovered" ].to_i,
+                                                                "Deaths" => oneRawValue[ "Deaths" ].to_i,
+                                                                "Latitude" => oneRawValue[ "Latitude" ].to_f,
+                                                                "Longitude" => oneRawValue[ "Longitude" ].to_f }
+        end
+        @daily  = Daily.new( :date => datestringIn, :territory => oneRawValue[ "Province/State" ], :territoryparent => oneRawValue[ "Country/Region" ],
+                          :summary => false, :confirmed => oneRawValue[ "Confirmed" ], :recovered => oneRawValue[ "Recovered" ],
+                          :deaths => oneRawValue[ "Deaths" ], :latitude => oneRawValue[ "Latitude" ].to_f, :longitude => oneRawValue[ "Longitude" ].to_f )
       else
         puts "one.Country/Region=" + oneRawValue[ "Country/Region" ] + "  confirmed=" + oneRawValue[ "Confirmed" ]
-        totalConfirmed  += oneRawValue[ "Confirmed" ].to_i
+        #totalConfirmed  += oneRawValue[ "Confirmed" ].to_i
+        @daily  = Daily.new( :date => datestringIn, :territory => oneRawValue[ "Country/Region" ], :territoryparent => KTerritoryWorld,
+                          :summary => false, :confirmed => oneRawValue[ "Confirmed" ], :recovered => oneRawValue[ "Recovered" ],
+                          :deaths => oneRawValue[ "Deaths" ], :latitude => oneRawValue[ "Latitude" ].to_f, :longitude => oneRawValue[ "Longitude" ].to_f )
+
+        totals[ "Confirmed" ] += oneRawValue[ "Confirmed" ].to_i
+        totals[ "Recovered" ] += oneRawValue[ "Recovered" ].to_i
+        totals[ "Deaths" ]    += oneRawValue[ "Deaths" ].to_i
       end
+
+      @daily.save
     end
 
     # Now for all those that saw accumulated province/state(s)
@@ -79,9 +114,23 @@ class WelcomeController < ApplicationController
     puts "countries with provinces/states"
     countrySummaries.each do |oneKey, oneValue|
       puts "one.accumulated.Country/Region=" + oneKey + " total confirmed=" + countrySummaries[ oneKey ][ "Confirmed" ].to_s
-      totalConfirmed += countrySummaries[ oneKey ][ "Confirmed" ]
+
+      @daily  = Daily.new( :date => datestringIn, :territory => oneKey, :territoryparent => KTerritoryWorld, :summary => true, :confirmed => oneValue[ "Confirmed" ].to_s,
+                          :recovered => oneValue[ "Recovered" ].to_s, :deaths => oneValue[ "Deaths" ].to_s, :latitude => oneValue[ "Latitude" ],
+                          :longitude => oneValue[ "Longitude" ] )
+      @daily.save
+
+      # totalConfirmed += countrySummaries[ oneKey ][ "Confirmed" ]
+      totals[ "Confirmed" ] += countrySummaries[ oneKey ][ "Confirmed" ]
+      totals[ "Recovered" ] += countrySummaries[ oneKey ][ "Recovered" ]
+      totals[ "Deaths" ]    += countrySummaries[ oneKey ][ "Deaths" ]
     end
 
-    return totalConfirmed
+    puts "index: totals.Confirmed,Recovered,Deaths=" + totals[ "Confirmed" ].to_s + "," + totals[ "Recovered" ].to_s + "," + totals[ "Deaths" ].to_s
+    @daily  = Daily.new( :date => datestringIn, :territory => KTerritoryWorld, :territoryparent => "(none)", :summary => true, :confirmed => totals[ "Confirmed" ].to_s,
+                        :recovered => totals[ "Recovered" ].to_s, :deaths => totals[ "Deaths" ].to_s, :latitude => 0.0, :longitude => 0.0 )
+    @daily.save
+
+    return totals[ "Confirmed" ]
   end
 end
