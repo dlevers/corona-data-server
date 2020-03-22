392cb1e3d793f2fff882ddfd0adb3ce8ddb5b99c
Dan Evers <dle1967@gmail.com>
2020-03-22 09:55:06 -0400

    Sort drill down by confirmed

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -6,10 +6,28 @@ class DailiesController < ApplicationController
 
     @dailies = Daily.where( :territoryparent => daily.territory )
 
-    allTerritories  = []
+    allTerritoriesWithCounts  = {}
     @dailies.each do |oneDaily|
-        allTerritories.append( oneDaily.territory )
+      # if countrySummaries.key?( oneRawValue[ "Country/Region" ])
+      if allTerritoriesWithCounts.key?( oneDaily.territory )
+        if oneDaily.confirmed.to_i > allTerritoriesWithCounts[ oneDaily.territory ]
+          allTerritoriesWithCounts[ oneDaily.territory ] = oneDaily.confirmed.to_i
+        end
+      else
+        allTerritoriesWithCounts[ oneDaily.territory ]  = oneDaily.confirmed.to_i
+        #allTerritoriesWithCounts.append({ "territory" => oneDaily.territory, "confirmed" => oneDaily.confirmed.to_i })
+      end
     end
-    @uTerritories = allTerritories.uniq()
+    #allTerritoriesWithCounts.sort_by{ |obj| obj[ "confirmed" ]}.reverse
+    sortedTerritoriesWithCounts  = allTerritoriesWithCounts.sort_by{ |k, v| -v }.to_h
+
+    # allTerritories  = []
+    # allTerritoriesWithCounts.each_key do |oneCountKey|
+    #   #allTerritories.append( oneWithCounts[ "territory" ])
+    #   allTerritories.append( oneCountKey )
+    # end
+
+    #@uTerritories = allTerritories.uniq()
+    @uTerritories = sortedTerritoriesWithCounts.keys.uniq
   end
 end
