9d6acdcd36db13cbc0c841749ff1a7c4453faf57
Dan Evers <dle1967@gmail.com>
2020-03-22 11:37:23 -0400

    Add drill down to states

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -8,26 +8,16 @@ class DailiesController < ApplicationController
 
     allTerritoriesWithCounts  = {}
     @dailies.each do |oneDaily|
-      # if countrySummaries.key?( oneRawValue[ "Country/Region" ])
       if allTerritoriesWithCounts.key?( oneDaily.territory )
         if oneDaily.confirmed.to_i > allTerritoriesWithCounts[ oneDaily.territory ]
           allTerritoriesWithCounts[ oneDaily.territory ] = oneDaily.confirmed.to_i
         end
       else
         allTerritoriesWithCounts[ oneDaily.territory ]  = oneDaily.confirmed.to_i
-        #allTerritoriesWithCounts.append({ "territory" => oneDaily.territory, "confirmed" => oneDaily.confirmed.to_i })
       end
     end
-    #allTerritoriesWithCounts.sort_by{ |obj| obj[ "confirmed" ]}.reverse
     sortedTerritoriesWithCounts  = allTerritoriesWithCounts.sort_by{ |k, v| -v }.to_h
 
-    # allTerritories  = []
-    # allTerritoriesWithCounts.each_key do |oneCountKey|
-    #   #allTerritories.append( oneWithCounts[ "territory" ])
-    #   allTerritories.append( oneCountKey )
-    # end
-
-    #@uTerritories = allTerritories.uniq()
     @uTerritories = sortedTerritoriesWithCounts.keys.uniq
   end
 end
