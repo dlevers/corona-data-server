3383aeb1d697383ce1ff62f6f175f58ad064de53
Dan Evers <dle1967@gmail.com>
2020-04-27 18:46:17 -0400

    Some code toward population-relative map

--- app/controllers/dailies_controller.rb
+++ app/controllers/dailies_controller.rb
@@ -19,5 +19,40 @@ class DailiesController < ApplicationController
     sortedTerritoriesWithCounts  = allTerritoriesWithCounts.sort_by{ |k, v| -v }.to_h
 
     @uTerritories = sortedTerritoriesWithCounts.keys.uniq
+
+
+    @populationCountries  = {}
+    @populationUSStates   = {}
+
+    populationDataPathBase = ENV[ 'DLE_CORONA_POPULATIONDATA_PATH' ] || "/Users/dlevers/Src/Sandbox/Corona/corona-data-server/data/Population"
+    puts "index: populationDataPathBase=" + populationDataPathBase
+    Dir.foreach( populationDataPathBase ) do |filename|
+      next if filename == '.' or filename == '..'
+
+      dest  = nil
+      case filename
+      when 'PopulationCountries202004.csv'
+        dest = 'countries'
+      when 'PopulationUSStates202004.csv'
+        dest = 'states'
+      else
+        puts "index: UNKNOWN population data filename=" + filename
+      end
+
+      unless dest.nil?
+        CSV.foreach(File.join(populationDataPathBase, filename), :headers => true) do |row|
+          # Moulding.create!(row.to_hash)
+          # puts "index: dest=" + dest + "  row=" + row.to_hash.to_s
+          case dest
+          when 'countries'
+            @populationCountries[row.to_hash["Country"]]  = row.to_hash
+          when 'states'
+            @populationUSStates[row.to_hash["State"]]     = row.to_hash
+          end
+        end
+      end
+    end
+    puts "index: Countries=" + @populationCountries.to_s
+    puts "index: USStates=" + @populationUSStates.to_s
   end
 end
