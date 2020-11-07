ae21a52ff07d4f74e6dc376308fb43f7dce0592d
Dan Evers <dle1967@gmail.com>
2020-11-07 13:50:43 -0500

    Map in ratio of confirmed cases of 60 days

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,5 +2,71 @@ class RatesController < ApplicationController
   def show
     daily = Daily.find( params[ :id ])
     puts "RatesController.show: daily.territory=" + daily.territory
+    @parentTerritory = daily.territory
+
+    @dailies = Daily.where( :territoryparent => daily.territory )
+
+    allTerritoriesWithCounts  = {}
+    @dailies.each do |oneDaily|
+      if allTerritoriesWithCounts.key?( oneDaily.territory )
+        if oneDaily.confirmed.to_i > allTerritoriesWithCounts[ oneDaily.territory ]
+          allTerritoriesWithCounts[ oneDaily.territory ] = oneDaily.confirmed.to_i
+        end
+      else
+        allTerritoriesWithCounts[ oneDaily.territory ]  = oneDaily.confirmed.to_i
+      end
+    end
+    sortedTerritoriesWithCounts  = allTerritoriesWithCounts.sort_by{ |k, v| -v }.to_h
+
+    @uTerritories = sortedTerritoriesWithCounts.keys.uniq
+
+
+    # @populationCountries    = {}
+    # @populationUSStates     = {}
+    # @populationUSFLCounties = {}
+
+    # populationDataPathBase = ENV[ 'DLE_CORONA_POPULATIONDATA_PATH' ] || "/Users/dlevers/Src/Sandbox/Corona/corona-data-server/data/Population"
+    # puts "RatesController.show: populationDataPathBase=" + populationDataPathBase
+    # Dir.foreach( populationDataPathBase ) do |filename|
+    #   next if filename == '.' or filename == '..'
+
+    #   dest  = nil
+    #   case filename
+    #   when 'PopulationCountries202004.csv'
+    #     dest = 'countries'
+    #   when 'PopulationUSStates202004.csv'
+    #     dest = 'states'
+    #   when 'PopulationUS-FLCounties202004.csv'
+    #     dest = 'counties'
+    #   else
+    #     puts "RatesController.show: UNKNOWN population data filename=" + filename
+    #   end
+
+    #   unless dest.nil?
+    #     CSV.foreach(File.join(populationDataPathBase, filename), :headers => true) do |row|
+    #       case dest
+    #       when 'countries'
+    #         @populationCountries[row.to_hash["Country"]]  = row.to_hash
+    #       when 'states'
+    #         @populationUSStates[row.to_hash["State"]]     = row.to_hash
+    #       when 'counties'
+    #         @populationUSFLCounties[row.to_hash["County"]]     = row.to_hash
+    #       end
+    #     end
+    #   end
+    # end
+    # puts "RatesController.show: Countries=" + @populationCountries.to_s
+    # puts "RatesController.show: USStates=" + @populationUSStates.to_s
+    # puts "RatesController.show: US-FLCounties=" + @populationUSFLCounties.to_s
+
+    # if @parentTerritory == "world"
+    #   @populationToUse = @populationCountries
+    # elsif @parentTerritory == "US"
+    #   @populationToUse = @populationUSStates
+    # elsif @parentTerritory == "Florida"
+    #   @populationToUse = @populationUSFLCounties
+    # else
+    #   @populationToUse = {}
+    # end
   end
 end
